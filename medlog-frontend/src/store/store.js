430bbc98b286979c715d5bd5dfef5987dbc78d62
Suchitha <suchithabolar.ganglia@gmail.com>
2025-03-08 19:46:01 +0530

    implemented redux to reports page

--- medlog-frontend/src/store/store.js
+++ medlog-frontend/src/store/store.js
@@ -1,13 +1,15 @@
+// store/store.js
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../reducers/authReducer";
 import logentryReducer from "../reducers/logentryReducer";
-import reportsReducer from "../reducers/reportsReducer"; // Import the new reducer
+import reportsReducer from "../reducers/reportsReducer";
 import categoryReducer from "../reducers/categoryReducer";
+
 const store = configureStore({
   reducer: {
     auth: authReducer,
     logentry: logentryReducer,
-    reports: reportsReducer, // Add reportsReducer to the store
+    reports: reportsReducer,
     category: categoryReducer,
   },
 });
